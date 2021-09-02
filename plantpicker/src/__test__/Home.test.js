2dcafdda00c7ba120932c9852d8df3f1f43d648d
Joyce-w <hellojoycewong@gmail.com>
2021-09-01 22:37:20 -0700

    write test for Home component

---
+++ plantpicker/src/__test__/Home.test.js
@@ -0,0 +1,35 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import {MemoryRouter} from 'react-router-dom'
+import Home from "../Home";
+
+it("renders w/o crashing", () => {
+    render(<MemoryRouter><Home/></MemoryRouter>)
+})
+
+it("matches snapshot", function () {
+    const { asFragment } = render(<MemoryRouter><Home /></MemoryRouter>);
+    expect(asFragment()).toMatchSnapshot();
+})
+
+test("if links are on the page", () => {
+    const { getByText } = render(<MemoryRouter><Home /></MemoryRouter>);
+
+    let signupBtn = getByText("Signup");
+    let quiz = getByText("Start Quiz");
+    let header = getByText("Plant + Pot");
+
+    expect(signupBtn).toBeInTheDocument();
+    expect(quiz).toBeInTheDocument();
+    expect(header).toBeInTheDocument();
+})
+
+test("redirects to correct component", () => {
+    const { getByText, getByLabelText, debug } = render(<MemoryRouter><Home /></MemoryRouter>);
+
+    //toggle button to direct to signup page
+    let signupBtn = getByText("Signup");
+    fireEvent.click(signupBtn);
+    debug()
+    // const inputNode = screen.getByLabelText('Email', {selector: 'input'})
+    // expect(inputNode).toBeInTheDocument();
+})
