d91cef52e13155e2dd0db1e5cedeb16028574927
Joyce-w <hellojoycewong@gmail.com>
2021-09-05 10:25:40 -0700

    add plant cards from userlist into List component

--- plantpicker/src/List.js
+++ plantpicker/src/List.js
@@ -3,6 +3,7 @@ import { useEffect, useState, useContext } from 'react';
 import { Redirect, useParams, Link } from 'react-router-dom';
 import PlantsApi from "./api";
 import { decodeToken } from "react-jwt";
+import "./List.css"
 
 function List(listData) {
     console.log(listData)
@@ -18,13 +19,8 @@ function List(listData) {
             const res = await PlantsApi.getList(id);
             console.log('plant_list', res.plants_list)
             setList(res.plants_list)
-            
-            // //get plant data and set in state
-            // let info = res.plants_list.map(async p => {return await PlantsApi.getPlant(p)});
-            // console.log(info)
+            getData(res.plants_list)
         }
-        getList(list_id);
-        getData(list)
 
          async function getData(plantList) {
             console.log('list', plantList)
@@ -35,42 +31,41 @@ function List(listData) {
                     [...plantInfo, res]
                 )
             }
-            
-                // setPlantInfo((plantInfo) => {
-                //     [...await PlantsApi.getPlant(p)]
-                // })
-            )
-        }
+                
+        )}
+
+        getList(list_id);
         
 
+
+
     }, [])
+    console.log('list', list)
+    console.log(plantInfo)
 
-    // useEffect(() => {
-    //     async function getData(plantList) {
-    //         console.log('list', plantList)
-
-    //         plantList.map(async (p) => {
-    //             let res = await PlantsApi.getPlant(p);
-    //             setPlantInfo((plantInfo) => 
-    //                 [...plantInfo, res]
-    //             )
-    //         }
-            
-    //             // setPlantInfo((plantInfo) => {
-    //             //     [...await PlantsApi.getPlant(p)]
-    //             // })
-    //         )
-    //     }
-    //     getData(list)
-
-    // },[list])
-    
 
     return (
         <div className="List">
-            
-        </div>
+            <h1>Plant List</h1>
+            <p>User information here</p>
+            <div className="List-card-container">
+                {plantInfo ?
+                    
+                    plantInfo.map(p =>
+                        <Link to={`/plants/${p.id}` }>
+                        <div class="Plant-card">
+                            <img src={p.img}className="Plant-card-img" alt={p.plant_name}></img>
+                            <div className="Plant-card-body">
+                                <h4>{p.plant_name}</h4>
+                            </div>
+                        </div>
+                        </Link>
+                    )
+                    : <p>No list present</p>
+                }
+            </div>
 
+        </div>
     )
 }
 
